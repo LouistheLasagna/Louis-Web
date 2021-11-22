cc048f30218c54c024da94fce2b97c34c5bccb29
LouistheLasagna <louisyou08@gmail.com>
2021-11-22 22:09:53 +0900

    Finished

---
+++ a-Quarter 2/homework3.js
@@ -0,0 +1,20 @@
+let racenumber=(Math.floor(Math.random()*1000))
+
+
+let timeregistered=true
+
+let runnerage=19
+
+if (runnerage>18, timeregistered===true){
+    racenumber+=1000
+}
+
+if (runnerage>18 && timeregistered===true){
+    console.log("You will race at 9:30 AM, your race number is", racenumber)
+} else if (runnerage>18 && timeregistered===false) {
+    console.log("You will race at 11:00 AM, your race number is", racenumber)
+} else if (runnerage<18 && timeregistered===true||false) {
+    console.log("You will race at 12:30 PM, your race number is", racenumber)
+} else (runnerage===18); {
+    console.log("Please see the reigsteration desk.")
+}
