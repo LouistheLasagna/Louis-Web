f0392025eb8e103252db3c8477c42fb5e28d86cf
LouistheLasagna <louisyou08@gmail.com>
2021-12-03 09:01:34 +0900

    Homework 4 finished

--- a-Quarter 2/scope.js
+++ a-Quarter 2/scope.js
@@ -22,3 +22,23 @@ const city=("Seoul")
 function logcityskyline (){
     let skyscraper=("63 building")
 }
+
+
+// const logskycolor=()=>{
+//     let color="blue"
+//     console.log(color)
+// }
+
+// logskycolor()
+// console.log(color) //reference error
+
+
+let num=50
+
+const lognum=()=>{
+    num=100
+    console.log(num)
+}
+
+lognum()
+console.log(num)
